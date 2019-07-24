f35adc7622409a729fe4dc16bfa940bcc8ec8043
Santiago Montiel <santimontiel@yahoo.com>
2019-07-23 22:22:24 -0300

    Agrego primetos tests a la SPA

--- test/cypress/integration/firstTest.js
+++ test/cypress/integration/firstTest.js
@@ -1,8 +1,23 @@
-'use strict'
-
 describe('Prueba Onboarding Principal',() => {
+
+    /* eslint-disable */
     it('Se debe mostrar la pantalla de home', ()=>{
-        // eslint-disable-next-line no-undef
         cy.visit('')
+    });
+
+    it('Hacer click sobre el boton Start',()=>{
+        cy.contains('Start').click()
+    });
+
+    it('Agregar un nuevo badge',()=>{
+        cy.visit('/badges/new');
+        cy.get(':nth-child(1) > .form-control').type('Lionel');
+        cy.get(':nth-child(2) > .form-control').type('Messi');
+        cy.get(':nth-child(3) > .form-control').type('lio@gmail.com');
+        cy.get(':nth-child(4) > .form-control').type('El mejor futbolista ever');
+        cy.get(':nth-child(5) > .form-control').type('LionelMessiOk');
+        cy.contains('Save').click();
+        cy.wait(3000);
     })
+
 });
