512e20609d58933a2ff9a94865382920c99726f7
Santiago Montiel <santimontiel@yahoo.com>
2019-07-27 22:18:35 -0300

    Agrego fixtures a los tests

--- test/cypress/integration/firstTest.js
+++ test/cypress/integration/firstTest.js
@@ -3,6 +3,11 @@ describe('Pruebas funcionales',() => {
     /* eslint-disable */
     before(() => {
         cy.log('Comienzo la suite de test');
+
+    });
+
+    beforeEach( () => {
+        cy.fixture('dataBadges.json').as('dataBadges');
     });
 
     it('Se debe mostrar la pantalla de home', ()=>{
@@ -14,26 +19,31 @@ describe('Pruebas funcionales',() => {
     });
 
     it('Agregar un nuevo badge',()=>{
-        cy.visit('/badges/new');
-        cy.get(':nth-child(1) > .form-control').type('Lionel');
-        cy.get(':nth-child(2) > .form-control').type('Messi');
-        cy.get(':nth-child(3) > .form-control').type('lio@gmail.com');
-        cy.get(':nth-child(4) > .form-control').type('El mejor futbolista ever');
-        cy.get(':nth-child(5) > .form-control').type('LionelMessiOk');
-        cy.contains('Save').click();
-        cy.wait(3000);
-        cy.contains('Lionel Messi').should('exist');
+        cy.get('@dataBadges').then((dataBadges) =>{
+            cy.visit('/badges/new');
+            cy.get(':nth-child(1) > .form-control').type(dataBadges.name);
+            cy.get(':nth-child(2) > .form-control').type(dataBadges.lastName);
+            cy.get(':nth-child(3) > .form-control').type(dataBadges.email);
+            cy.get(':nth-child(4) > .form-control').type(dataBadges.job);
+            cy.get(':nth-child(5) > .form-control').type(dataBadges.instagram);
+            cy.contains('Save').click();
+            cy.wait(3000);
+            cy.contains('Lionel Messi').should('exist');
+        });
+
     });
 
     it('Eliminar un badge',()=>{
-        cy.visit('/badges');
-        cy.contains('Lionel Messi').should('exist');
-        cy.get(':nth-child(53) > .text-reset > .BadgesListItem').click();
-        cy.get(':nth-child(3) > .btn').click();
-        cy.get('.DeleteBadgeModal > div > .btn-danger').click();
-        cy.wait(3000);
-        cy.contains('Lionel Messi').should('not.exist');
-    })
+        cy.get('@dataBadges').then((dataBadges) =>{
+            cy.visit('/badges');
+            cy.contains(dataBadges.name + ' ' + dataBadges.lastName).should('exist');
+            cy.get(':nth-child(53) > .text-reset > .BadgesListItem').click();
+            cy.get(':nth-child(3) > .btn').click();
+            cy.get('.DeleteBadgeModal > div > .btn-danger').click();
+            cy.wait(3000);
+            cy.contains('Lionel Messi').should('not.exist');
+        });
+    });
 
     after(() =>{
         cy.log('test finalizados');
