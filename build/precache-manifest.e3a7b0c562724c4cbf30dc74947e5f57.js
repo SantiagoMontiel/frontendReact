f2d06632781b8365f20d22ea63b296028947c715
Santiago Montiel <santimontiel@yahoo.com>
2019-10-19 23:16:02 -0300

    Elimino la carpeta cypress fuera de test y todo su contenido

---
+++ build/precache-manifest.e3a7b0c562724c4cbf30dc74947e5f57.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "a1880fbfb5634577b88090c9f82b3de7",
+    "url": "/static/media/platziconf-logo.a1880fbf.svg"
+  },
+  {
+    "revision": "20fd085901c841de2a1f",
+    "url": "/static/css/main.cc6529d0.chunk.css"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "583bbdbabe2f80a25396e12e00bf3d56",
+    "url": "/static/media/back-home.583bbdba.jpg"
+  },
+  {
+    "revision": "303608570708b5db5096",
+    "url": "/static/js/2.30360857.chunk.js"
+  },
+  {
+    "revision": "be144abf8a8a7019e6fbc4c27f71cc48",
+    "url": "/static/media/Banco_galicia_logo.be144abf.png"
+  },
+  {
+    "revision": "20fd085901c841de2a1f",
+    "url": "/static/js/main.20fd0859.chunk.js"
+  },
+  {
+    "revision": "0d2ff286deb3895d3ee82fed78c4dce4",
+    "url": "/static/media/astronauts.0d2ff286.svg"
+  },
+  {
+    "revision": "ed7c58eafe17eff3b2557c0369b20264",
+    "url": "/static/media/badge-header.ed7c58ea.svg"
+  },
+  {
+    "revision": "0abcdfcd1e323649a0338a204fe8b853",
+    "url": "/static/media/stars.0abcdfcd.svg"
+  },
+  {
+    "revision": "303608570708b5db5096",
+    "url": "/static/css/2.81237299.chunk.css"
+  },
+  {
+    "revision": "4af705ef5d0d8782b91c678d93b914c3",
+    "url": "/index.html"
+  }
+];
