50d5a43c6c3d0d1ba013f5c518899dbdb4bb334c
Santiago Montiel <santimontiel@yahoo.com>
2019-10-21 22:48:18 -0300

    Refactor

---
+++ build/precache-manifest.5b2113e4ec767ced98768d18ac20f2e1.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "546d9838bb7fc70eb81ab0e2205750f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db5551eb2195060eb97b",
+    "url": "/static/css/2.0f7a873c.chunk.css"
+  },
+  {
+    "revision": "966c4476a5bb0b0d3390",
+    "url": "/static/css/main.8b053fc4.chunk.css"
+  },
+  {
+    "revision": "db5551eb2195060eb97b",
+    "url": "/static/js/2.db3a545e.chunk.js"
+  },
+  {
+    "revision": "966c4476a5bb0b0d3390",
+    "url": "/static/js/main.edba4ac2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0d2ff286deb3895d3ee82fed78c4dce4",
+    "url": "/static/media/astronauts.0d2ff286.svg"
+  },
+  {
+    "revision": "ed7c58eafe17eff3b2557c0369b20264",
+    "url": "/static/media/badge-header.ed7c58ea.svg"
+  },
+  {
+    "revision": "6841730f1bbe3235e7a0cdc7d244d244",
+    "url": "/static/media/logo.6841730f.svg"
+  },
+  {
+    "revision": "a1880fbfb5634577b88090c9f82b3de7",
+    "url": "/static/media/platziconf-logo.a1880fbf.svg"
+  },
+  {
+    "revision": "0abcdfcd1e323649a0338a204fe8b853",
+    "url": "/static/media/stars.0abcdfcd.svg"
+  }
+]);
